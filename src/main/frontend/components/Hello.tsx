d5fe072a5f85beb3d04f27cb159f0789e9fc3563
Andreas Bernerus <andreas.bernerus@consultant.volvo.com>
2017-03-22 15:56:43 +0100

    Typescript and webpack

---
+++ src/main/frontend/components/Hello.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+export interface HelloProps { compiler: string; framework: string }
+
+//export const Hello = (props: HelloProps) => <h1>Hello from {props.compiler} and {props.framework}!</h1>;
+
+export class Hello extends React.Component<HelloProps, undefined> {
+    render() {
+        return <h1>Yoyo from {this.props.compiler} and {this.props.framework}!</h1>;
+    }
+}
